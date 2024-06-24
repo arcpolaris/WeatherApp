9b81b549db423abfbeea6c15f6d415d61c35dbf7
arcpolaris <143139215+arcpolaris@users.noreply.github.com>
2024-06-24 16:48:07 -0700

    weather api test

---
+++ WeatherServices/debug.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "https://weatherapp-8jw4.onrender.com/api"
+
+
+data = {"query": "point", "latitude": "47.5430912", "longitude": "-122.0149248"}
+response = requests.post(url, json=data)
+
+print(f"Status Code: {response.status_code}")
+print(f"Response JSON: {response.json()}")
