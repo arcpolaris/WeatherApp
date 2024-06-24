a777ce98f6e8eae4a38a897f4f84d2e88f4e10b3
arcpolaris <143139215+arcpolaris@users.noreply.github.com>
2024-06-24 16:48:07 -0700

    weather api test

--- WeatherServices/app.py
+++ WeatherServices/app.py
@@ -1,18 +1,23 @@
+import requests
 from flask import Flask, request, jsonify
 
 app = Flask(__name__.split(".")[0])
 
+url = "https://api.weather.gov/"
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+# @app.route("/")
+# def hello_world():
+#    return "Hello, World!"
 
 
 @app.route("/api", methods=["POST"])
 def handle_request():
     data = request.get_json()
+    query = data["query"]
+    if query == "point":
+        return requests.get(f"{url}/points/{data['latitude']},{data['longitude']}")
     return jsonify(data)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#    app.run(debug=True)
