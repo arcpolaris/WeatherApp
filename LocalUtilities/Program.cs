950b8803c4e4aced3d5feb7d19c1c5f675056a1d
arcpolaris <143139215+arcpolaris@users.noreply.github.com>
2024-07-04 19:06:02 -0700

    Imported Siren Data for Settings Page - Created Settings Page - Added LocalUtilities Project

---
+++ LocalUtilities/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LocalUtilities
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\dsrivastava\source\repos\WeatherApp\LocalUtilities\SirenData.csv";
+            string dumpPath = @"C:\Users\dsrivastava\source\repos\WeatherApp\LocalUtilities\SirenData.json";
+            CSVHelper csv = new(sourcePath);
+            //var res = csv.HeaderTable;
+            //foreach (var entry in res)
+            //{
+            //    Console.Write(entry.Key + ": ");
+            //    foreach (var item in entry.Value) Console.Write(item + ", ");
+            //    Console.WriteLine();
+            //}
+            File.WriteAllText(dumpPath, csv.ToJson());
+        }
+    }
+}
---
+++ WeatherApp/SettingsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace WeatherApp;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+}
