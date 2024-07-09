57839abc9ae90b04ba6ba37a6ffe994f455f77ee
arcpolaris <143139215+arcpolaris@users.noreply.github.com>
2024-07-09 12:36:02 -0700

    Fixed the json data

--- LocalUtilities/CSVHelper.cs
+++ LocalUtilities/CSVHelper.cs
@@ -4,73 +4,73 @@ using System.Text.Json;
 
 namespace LocalUtilities
 {
-    internal class CSVHelper
-    {
-        private List<string[]> raw = new();
+	internal class CSVHelper
+	{
+		private List<string[]> raw = new();
 
-        public CSVHelper(string path)
-        {
-            using TextFieldParser parser = new(path);
-            parser.TextFieldType = FieldType.Delimited;
-            parser.SetDelimiters(",");
-            while (!parser.EndOfData)
-                raw.Add(parser.ReadFields() ?? []);
-            raw.RemoveAll(arr => (arr.Length == 0 || arr.All(item => string.IsNullOrWhiteSpace(item))));
-        }
+		public CSVHelper(string path)
+		{
+			using TextFieldParser parser = new(path);
+			parser.TextFieldType = FieldType.Delimited;
+			parser.SetDelimiters(",");
+			while (!parser.EndOfData)
+				raw.Add(parser.ReadFields() ?? []);
+			raw.RemoveAll(arr => (arr.Length == 0 || arr.All(item => string.IsNullOrWhiteSpace(item))));
+		}
 
-        public string[,] Grid
-            => raw.SelectMany((fields, rowIndex)
-            => fields.Select((field, colIndex)
-            => new { field, rowIndex, colIndex }))
-            .Aggregate(new string[raw.Count, raw[0].Length], (res, x) =>
-            {
-                res[x.rowIndex, x.colIndex] = x.field;
-                return res;
-            });
+		public string[,] Grid
+			=> raw.SelectMany((fields, rowIndex)
+			=> fields.Select((field, colIndex)
+			=> new { field, rowIndex, colIndex }))
+			.Aggregate(new string[raw.Count, raw[0].Length], (res, x) =>
+			{
+				res[x.rowIndex, x.colIndex] = x.field;
+				return res;
+			});
 
-        public Dictionary<string, string[]> DataTable
-            => raw[0]
-            .Skip(1)
-            .Select((header, colIndex)
-            => new KeyValuePair<string, string[]>(
-                header,
-                raw.Skip(1)
-                    .Select(row => row[colIndex])
-                    .TakeWhile(field => !string.IsNullOrWhiteSpace(field))
-                    .ToArray()))
-            .ToDictionary(pair => pair.Key, pair => pair.Value);
-        public Dictionary<string, string[]> HeaderTable
-            => raw
-            .Select(row => row.First())
-            .Skip(1)
-            .TakeWhile(field => !string.IsNullOrWhiteSpace(field))
-            .Aggregate(
-                new List<List<string>> { new() },
-                (acc, item) =>
-                {
-                    if (Regex.Match(item, "[A-Z]{2}").Success && acc.Last().Count > 1)
-                        acc.Add([item]);
-                    else
-                        acc.Last().Add(item);
-                    return acc;
-                }
-                )
-            .Where(sublist => sublist.Count > 0)
-            .Select((item)
-                => new KeyValuePair<string, string[]>(item.First(), item.Skip(1).ToArray()))
-            .ToDictionary(pair => pair.Key, pair => pair.Value);
+		public Dictionary<string, string[]> DataTable
+			=> raw[0]
+			.Skip(1)
+			.Select((header, colIndex)
+			=> new KeyValuePair<string, string[]>(
+				header,
+				raw.Skip(1)
+					.Select(row => row[colIndex + 1])
+					.TakeWhile(field => !string.IsNullOrWhiteSpace(field))
+					.ToArray()))
+			.ToDictionary(pair => pair.Key, pair => pair.Value);
+		public Dictionary<string, string[]> HeaderTable
+			=> raw
+			.Select(row => row.First())
+			.Skip(1)
+			.TakeWhile(field => !string.IsNullOrWhiteSpace(field))
+			.Aggregate(
+				new List<List<string>> { new() },
+				(acc, item) =>
+				{
+					if (Regex.Match(item, "[A-Z]{2}").Success && acc.Last().Count > 1)
+						acc.Add([item]);
+					else
+						acc.Last().Add(item);
+					return acc;
+				}
+				)
+			.Where(sublist => sublist.Count > 0)
+			.Select((item)
+				=> new KeyValuePair<string, string[]>(item.First(), item.Skip(1).ToArray()))
+			.ToDictionary(pair => pair.Key, pair => pair.Value);
 
-        public string ToJson()
-        {
-            Dictionary<string, object> dict = new(){
-                {"counties", HeaderTable},
-                {"cities", DataTable}
-            };
-            JsonSerializerOptions options = new()
-            {
-                WriteIndented = false
-            };
-            return JsonSerializer.Serialize(dict, options);
-        }
-    }
+		public string ToJson()
+		{
+			Dictionary<string, object> dict = new(){
+				{"counties", HeaderTable},
+				{"cities", DataTable}
+			};
+			JsonSerializerOptions options = new()
+			{
+				WriteIndented = false
+			};
+			return JsonSerializer.Serialize(dict, options);
+		}
+	}
 }
