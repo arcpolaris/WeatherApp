ecce44cb21f0f11101b02cb17bc7b41199f521be
arcpolaris <143139215+arcpolaris@users.noreply.github.com>
2024-07-15 14:50:16 -0700

    IDEK

--- WeatherApp/MapsPage.xaml.cs
+++ WeatherApp/MapsPage.xaml.cs
@@ -1,15 +1,30 @@
+using System.Collections.ObjectModel;
+using static System.Tuple;
+
 namespace WeatherApp;
 
 public partial class MapsPage : ContentPage
 {
+	public ObservableCollection<Tuple<string, string>> Queries { get; set; } = [];
+	readonly static List<Tuple<string, string>> _queries = [
+		Create("Hospitals Near Me", "Hospital"),
+		Create("Vets Near Me", "Veterinarian"),
+		Create("Car Repair", "Mechanic"),
+		Create("Insurance Near Me", "Insurance Service"),
+		Create("Roofers" , "Roofers"),
+		Create("Siding Services", "Siding, Service")
+	];
+	public Command<string> MapCommand { get; set; } = new(OpenMap);
+
 	public MapsPage()
 	{
 		InitializeComponent();
+		BindingContext = this;
+		_queries.ForEach(item => Queries.Add(item));
 	}
 
-	public async void OpenMap(object sender, EventArgs e)
+	public static async void OpenMap(string query)
 	{
-		string query = "shops";
 		Uri uri = new($"https://www.google.com/maps/search/?api=1&query={Uri.EscapeDataString(query)}");
 		await Launcher.OpenAsync(uri);
 	}
