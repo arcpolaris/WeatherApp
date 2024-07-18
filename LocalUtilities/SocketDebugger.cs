8b236fbea4358e69c94ddfd7998f4f238e0c25e8
arcpolaris <143139215+arcpolaris@users.noreply.github.com>
2024-07-18 12:03:26 -0700

    back to debugging...

--- LocalUtilities/SocketDebugger.cs
+++ LocalUtilities/SocketDebugger.cs
@@ -1,6 +1,7 @@
 ﻿#pragma warning disable CS1998
 
 using SocketIOClient;
+using System.Text;
 
 namespace LocalUtilities
 {
@@ -20,13 +21,30 @@ namespace LocalUtilities
 			});
 
 			await client.ConnectAsync();
+			await Request();
 			Console.WriteLine("Type Quit to exit...");
 			while (Console.ReadLine()?.ToLower().Trim() != "quit") ;
 			await client.DisconnectAsync();
 		}
+
+		static async Task Request()
+		{
+			string jsonPayload = "{\"text\":\"hello world\"}";
+
+			using var client = new HttpClient();
+			var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+			try
+			{
+				var response = await client.PostAsync($"{url}/publish", content);
+				string responseContent = await response.Content.ReadAsStringAsync();
+				Console.WriteLine($"Response Status Code: {response.StatusCode}");
+				Console.WriteLine($"Response Content: {responseContent}");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error: {ex.Message}");
+			}
+		}
 	}
 }
-
-/*
-curl POST https://weatherapp-8jw4.onrender.com/publish --data '{}' -H "Content-type: application/json" -v
-*/
