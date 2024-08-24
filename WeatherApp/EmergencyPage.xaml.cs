fb4a9472c2e8ee62b1e1d11ea851ebb9f31c373c
arcpolaris <143139215+arcpolaris@users.noreply.github.com>
2024-08-24 13:26:27 -0700

    Replaced pdf viewer with text

--- WeatherApp/EmergencyPage.xaml.cs
+++ WeatherApp/EmergencyPage.xaml.cs
@@ -1,16 +1,21 @@
+using System.Diagnostics;
+using System.Windows.Input;
 using Microsoft.Maui.Controls.PlatformConfiguration.AndroidSpecific;
 
 namespace WeatherApp;
 
 public partial class EmergencyPage : ContentPage
 {
-	static readonly string url = "https://drive.google.com/file/d/14rYgRmPwjvWvQ97BPJ8_wazbjz2-KhVP/preview";
+	//static readonly string url = "https://drive.google.com/file/d/14rYgRmPwjvWvQ97BPJ8_wazbjz2-KhVP/preview";
+
+	public ICommand OpenPage => new Command(async () => await Launcher.OpenAsync(@"https://www.weather.gov/mkx/nwr-table"));
 	public EmergencyPage()
 	{
 		InitializeComponent();
-		pdfWebView.Source = url;
-		pdfWebView.On<Microsoft.Maui.Controls.PlatformConfiguration.Android>()
-			.SetDisplayZoomControls(true)
-			.SetEnableZoomControls(true);
+		BindingContext = this;
+		//pdfWebView.Source = url;
+		//pdfWebView.On<Microsoft.Maui.Controls.PlatformConfiguration.Android>()
+		//	.SetDisplayZoomControls(true)
+		//	.SetEnableZoomControls(true);
 	}
 }
